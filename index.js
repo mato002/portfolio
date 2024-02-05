fb3d923745749461667242fc5b2d144cc23710cf
mato002 <mathiasodhis@gmail.com>
2024-02-05 16:15:03 +0300

    matech

--- index.js
+++ index.js
@@ -31,10 +31,20 @@ function closeMenu(){
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxsNWCBGFNAB2jeqJGPoPER1PqbC03Duj6NNmkTJhDPgx8YssQGF5sgYEZFG0uZjaL0/exec'
         const form = document.forms['submit-to-google-sheet']
+        const msg = document.getElementById("msg")
       
         form.addEventListener('submit', e => {
           e.preventDefault()
           fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => console.log('Success!', response))
-            .catch(error => console.error('Error!', error.message))
+            .then(response => {
+                msg.innerHTML="Message sent successfully";
+                setTimeout(function(){
+                    msg.innerHTML = " "
+
+                },5000)
+
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.Message))
+        
         })
